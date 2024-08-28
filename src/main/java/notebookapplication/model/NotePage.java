7f0ff5421475e3da184d2f5828117b095d7bcdb9
Gustave <gustaverolland777@gmail.com>
2024-08-28 17:35:42 +0200

    continuing to optimise the implementation of NotePage classes, and also passing the tests

--- src/main/java/notebookapplication/model/NotePage.java
+++ src/main/java/notebookapplication/model/NotePage.java
@@ -1,6 +1,8 @@
 package notebookapplication.model;
 
 import java.beans.PropertyChangeSupport;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Represents a note page in a note group in the notebook application.
@@ -35,21 +37,21 @@ public class NotePage extends ModelObserver {
      *
      * @param caretPosition the position where user chose to insert the content
      * @param content   the content to be inserted
-     * @pre {@code caretPosition \in {0, ..., content.length() - 1}}
+     * @pre {@code caretPosition \in {0, ..., content.length()}}
      * @throws IllegalArgumentException if caretPosition is not inside the {@code content}
      *      string bounds
      */
     public void insertContent(int caretPosition, String content) {
-        if (caretPosition < 0 || caretPosition > content.length()) {
+        if (caretPosition < 0 || caretPosition > this.content.length()) {
             throw new IllegalArgumentException(
                     STR."Caret position \{caretPosition} is out of bounds");
         }
 
-        String oldContent = this.content.toString();
+        String oldContent = getContent();
         this.content.insert(caretPosition, content);
 
-        // notify the observer, sending both old and new values
-        support.firePropertyChange("content", oldContent, this.content.toString());
+        // notify the observer with Property Name "insert"
+        support.firePropertyChange("insert", oldContent, getContent());
     }
 
     /**
@@ -59,22 +61,23 @@ public class NotePage extends ModelObserver {
      *
      * @param startPosition the start position of the content to be deleted
      * @param endPosition   the end position of the content to be deleted
-     * @pre {@code startPosition, endPosition \in {0, ..., this.content.length() - 1}} &&
+     * @pre {@code startPosition, endPosition \in {0, ..., this.content.length()}} &&
      *      {@code startPosition < endPosition}
      * @throws IllegalArgumentException if precondition is violated
      */
     public void deleteContent(int startPosition, int endPosition) {
         if (startPosition < 0
                 || startPosition >= endPosition
-                || endPosition >= this.content.length()) {
-            throw new IllegalArgumentException("Position indexes for deletion is out of bounds");
+                || endPosition > this.content.length()) {
+            throw new IllegalArgumentException(
+                    "Position indexes for deletion is out of bounds");
         }
 
-        String oldContent = this.content.toString();
+        String oldContent = getContent();
         this.content.delete(startPosition, endPosition);
 
-        // notify the observer
-        support.firePropertyChange("content", oldContent, this.content.toString());
+        // notify the observer with Property Name "delete"
+        support.firePropertyChange("delete", oldContent, this.content.toString());
     }
 
     /**
@@ -85,13 +88,21 @@ public class NotePage extends ModelObserver {
      * @param startPosition the start position of the content to be replaced
      * @param endPosition   the end position of the content to be replaced
      * @param newContent    the new content to be inserted
-     * @pre satisfy the preconditions of both {@code deleteContent} and {@code insertContent} method
-     * @throws IllegalArgumentException if precondition of {@code deleteContent} or
-     *      {@code insertContent} has been violated
+     * @pre {@code startPosition, endPosition \in {0, this.content.length()}} &&
+     *      {@code startPosition < endPosition}
+     * @throws IllegalArgumentException if precondition is violated
      */
     public void replaceContent(int startPosition, int endPosition, String newContent) {
-        deleteContent(startPosition, endPosition);
-        insertContent(startPosition, newContent);
+        if (startPosition < 0
+                || startPosition >= endPosition
+                || endPosition > this.content.length()) {
+            throw new IllegalArgumentException(
+                    "Position indexes for deletion is out of bounds");
+        }
+
+        String oldContent = getContent();
+        content.replace(startPosition, endPosition, newContent);
+        support.firePropertyChange("replace", oldContent, getContent());
     }
 
     /**
@@ -102,27 +113,100 @@ public class NotePage extends ModelObserver {
      * @param lineNum   the line number where the symbol will be inserted
      * @param symbolEnum    the enum containing the symbol to be inserted
      * @pre {@code lineNum \in {0, ..., lines.length - 1}}
-     * @throws IllegalArgumentException if {@code lineNum} is out of bounds of lines
+     * @throws IllegalArgumentException if {@code lineNum} is out of bounds of {@code lines}
      * @post    {@code symbolEnum.symbol} is inserted to the front of the chosen line
      *      with two spaces between the symbol and the start of the text,
      *      while the other lines are not modified
      */
     public void insertSymbol(int lineNum, Symbols symbolEnum) {
-        String oldContent = this.content.toString();
+        String oldContent = getContent();
         String[] lines = content.toString().split("\n");
 
         if (lineNum < 0 || lineNum >= lines.length) {
-            throw new IllegalArgumentException(STR."Line number \{lineNum} is out of bounds");
+            throw new IllegalArgumentException(
+                    STR."Line number \{lineNum} is out of bounds");
         }
 
         lines[lineNum] = STR."\{symbolEnum.symbol}  \{lines[lineNum]}";
         content = new StringBuilder(String.join("\n", lines));
-        support.firePropertyChange("content", oldContent, content.toString());
+
+        // notify the observer with Property Name "symbol"
+        support.firePropertyChange("symbol", oldContent, getContent());
+    }
+
+    /**
+     * Replace the selected string with the same string but formatted with the
+     *      custom style tag specified in {@link TextEditing} class.
+     * Only accepting a lambda expression that takes the original content as input
+     *      and returns a formatted string for the parameter {@code stringSupplier}.
+     *
+     * <p>Example usage:</p>
+     * <pre>{@code
+     * NotePage notePage = new NotePage("pageName", "Some default content");
+     * // the indexes parsed to the substring inside the lambda function should
+     * // match the indexes parsed to the formatting function
+     * notePage.formatting(5, 12,
+     *      () -> TextEdit.setBold(notePage.getContent().substring(5, 12)));
+     * System.out.println(notePage.getContent());
+     * // should prints "Some [style="-fx-font-weight: bold;"]default[/style] content"
+     * }</pre>
+     *
+     * @param startPosition the start position of the content to be formatted
+     * @param endPosition   the end position of the content to be formatted
+     * @param stringSupplier   the lambda function that returns the string
+     *                         processed by one of the functions in {@link TextEditing}
+     * @pre {@code startPosition, endPosition \in {0, ..., this.content.length()}} &&
+     *      {@code startPosition < endPosition} && {@code stringSupplier.get()} contains the
+     *      correct custom style tag
+     * @throws IllegalArgumentException if any precondition is violated
+     * @post the selected content is surrounded with the custom style tag,
+     *      but the content itself is not modified
+     */
+    public void formatting(int startPosition,
+                           int endPosition,
+                           StringSupplier stringSupplier) {
+        if (startPosition < 0
+                || startPosition >= endPosition
+                || endPosition > this.content.length()) {
+            throw new IllegalArgumentException(
+                    "Position indexes for deletion is out of bounds");
+        }
+
+        String formattedString = stringSupplier.get();
+        // check if formattedString contains the correct custom style tag
+        if (!matchTag(formattedString)) {
+            throw new IllegalArgumentException("Unsupported formatting String");
+        }
+
+        String oldContent = getContent();
+        content.replace(startPosition, endPosition, formattedString);
+
+        // notify the observer with Property Name "format"
+        support.firePropertyChange("format", oldContent, getContent());
+    }
+
+    /**
+     * Private auxiliary method to help {@code formatting} function to check
+     *      if the formatted string parsed in contains the correct
+     *      custom style tag specified in {@link TextEditing} class.
+     *
+     * @param formattedString   the string that should contain the custom style tag
+     * @return  true if {@code formattedString} matches the pattern, false otherwise
+     */
+    private boolean matchTag(String formattedString) {
+        /*
+        Create the pattern of [style="..."]...[/style], where no other quotation mark
+            is allowed inside the first ellipsis.
+        The (?s) flag ensures the ".*?" matches the possible new lines
+            inside the second ellipsis.
+         */
+        Pattern pattern = Pattern.compile("(?s)\\[style=\"[^\"]*\"].*?\\[/style]");
+        return pattern.matcher(formattedString).matches();
     }
 
     // getter for string content of this.content
     public String getContent() {
-        return content.toString();
+        return this.content.toString();
     }
 
     // setter for this.content
@@ -132,11 +216,19 @@ public class NotePage extends ModelObserver {
 
     // getter for the page name
     public String getPageName() {
-        return pageName;
+        return this.pageName;
     }
 
-    // setter for the page name
+    /**
+     * Setter for the page name.
+     * Replace the page name with the new one, and also need to notify the observer.
+     *
+     * @param pageName  the page name to replace {@code this.pageName}
+     */
     public void setPageName(String pageName) {
+        // notify the observer with Property Name "pageName"
+        support.firePropertyChange("pageName", this.pageName, pageName);
+
         this.pageName = pageName;
     }
 }
--- src/test/java/notebookapplication/model/NotePageTest.java
+++ src/test/java/notebookapplication/model/NotePageTest.java
@@ -1,4 +1,184 @@
 package notebookapplication.model;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+/**
+ *
+ */
 public class NotePageTest {
+    private static final String DEFAULT_PAGE_NAME = "Default Page Name";
+    private static final String CONTENT =
+            """
+            This is the first line.
+            This is a second line...""";
+    private NotePage notePage;
+
+    /**
+     *
+     */
+    @BeforeEach
+    public void setUp() {
+         notePage = new NotePage(DEFAULT_PAGE_NAME, CONTENT);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testConstructor() {
+        assertAll(() -> {
+                    assertEquals(DEFAULT_PAGE_NAME, notePage.getPageName());
+                    assertEquals(CONTENT, notePage.getContent());
+                }
+        );
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void insertToTheEnd() {
+        String[] toBeInserted = {"Some inserted string", "\nthe second inserted string"};
+
+        notePage.insertContent(notePage.getContent().length(), toBeInserted[0]);
+        System.out.println(notePage.getContent());
+        assertEquals(STR."\{CONTENT}\{toBeInserted[0]}", notePage.getContent());
+
+        notePage.insertContent(notePage.getContent().length(), toBeInserted[1]);
+        System.out.println(notePage.getContent());
+        assertEquals(STR."\{CONTENT}\{toBeInserted[0]}\{toBeInserted[1]}", notePage.getContent());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void insertByChar() {
+        String inserted = "\nI'm inserting this line of string";
+        char[] toBeInserted = inserted.toCharArray();
+
+        for (var character : toBeInserted) {
+            notePage.insertContent(notePage.getContent().length(), String.valueOf(character));
+        }
+        System.out.println(notePage.getContent());
+        assertEquals(STR."\{CONTENT}\{inserted}", notePage.getContent());
+
+        for (int i = 0; i < toBeInserted.length; i++) {
+            notePage.insertContent(i, String.valueOf(toBeInserted[i]));
+        }
+        notePage.insertContent(inserted.length(), "\n");
+        System.out.println(notePage.getContent());
+        assertEquals(STR."\{inserted}\n\{CONTENT}\{inserted}", notePage.getContent());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void insertToAnywhere() {
+        String toBeInserted = "\nThis line should be inserted\n";
+        notePage.insertContent(0, toBeInserted);
+        notePage.insertContent(notePage.getContent().length() - 14,
+                toBeInserted);
+        notePage.insertContent(toBeInserted.length() + 8,
+                toBeInserted);
+        System.out.println(notePage.getContent());
+        assertEquals(
+                STR."""
+                        \{toBeInserted}This is \{toBeInserted}the first line.
+                        This is a \{toBeInserted}second line...""",
+                notePage.getContent()
+        );
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void insertContentException() {
+        assertAll(() -> {
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.insertContent(-1, "haha"));
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.insertContent(notePage.getContent().length() + 1, "haha"));
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.insertContent(-10, "haha"));
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.insertContent(300, "haha"));
+        });
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void deleteByChar() {
+        notePage.deleteContent(notePage.getContent().length() - 1, notePage.getContent().length());
+        notePage.deleteContent(0, 1);
+        System.out.println(notePage.getContent());
+        assertEquals("his is the first line.\nThis is a second line..", notePage.getContent());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void deleteByString() {
+        notePage.deleteContent(0, 23);
+        System.out.println(notePage.getContent());
+        assertEquals("\nThis is a second line...", notePage.getContent());
+
+        notePage.deleteContent(0, notePage.getContent().length());
+        assertTrue(notePage.getContent().isEmpty());
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void deleteContentException() {
+        assertAll(() -> {
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.deleteContent(-1, notePage.getContent().length()));
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.deleteContent(0, notePage.getContent().length() + 1));
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.deleteContent(5, 0));
+        });
+    }
+
+    @Test
+    public void replaceString() {
+        String replacement = "replaced by this string";
+        notePage.replaceContent(8, 31, replacement);
+        System.out.println(notePage.getContent());
+        assertEquals(STR."This is \{replacement} a second line...", notePage.getContent());
+    }
+
+    @Test
+    public void replaceAll() {
+        String replacement = "replaced by this string";
+        notePage.replaceContent(0, notePage.getContent().length(), replacement);
+        assertEquals(replacement, notePage.getContent());
+    }
+
+    @Test
+    public void replaceContentException() {
+        assertAll(() -> {
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.replaceContent(
+                            -1, notePage.getContent().length(), "haha"));
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.replaceContent(
+                            0, notePage.getContent().length() + 1, "haha"));
+            assertThrows(IllegalArgumentException.class,
+                    () -> notePage.replaceContent(5, 0, "haha"));
+        });
+    }
+
+    @Test
+    public void insertSingleSymbol() {
+        // ...
+    }
 }
