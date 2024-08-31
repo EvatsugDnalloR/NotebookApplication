d14b9dff7fb9da1ca0c6e7713b3237f8c0237e79
Gustave <gustaverolland777@gmail.com>
2024-08-31 13:51:07 +0200

    made the NoteFacade also extends ModelObserver for arranging list of NoteGroups

--- src/main/java/notebookapplication/model/NoteFacade.java
+++ src/main/java/notebookapplication/model/NoteFacade.java
@@ -2,7 +2,7 @@ package notebookapplication.model;
 
 import java.util.List;
 
-public class NoteFacade {
+public class NoteFacade extends ModelObserver {
     private List<NoteGroup> noteGroups;
 
     NoteFacade() {
