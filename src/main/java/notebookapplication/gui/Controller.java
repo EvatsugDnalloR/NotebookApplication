115ff60c06d7146f26cf9d626f934f743dca8489
Gustave <gustaverolland777@gmail.com>
2026-07-06 18:58:16 +0200

    found bug with rename undo/redo features

--- src/main/java/notebookapplication/gui/Controller.java
+++ src/main/java/notebookapplication/gui/Controller.java
@@ -37,6 +37,8 @@ import notebookapplication.model.NotePage;
  * <p>Implements both Initializable and PropertyChangeListener interfaces.
  */
 public class Controller implements Initializable, PropertyChangeListener {
+    // TODO: Fix Rename Undo/Redo features
+
     private static final Logger LOGGER = Logger.getLogger(Controller.class.getName());
 
     /**
