c8f908ceeeee7e4c3b7ac9769bf1af9baafa6acb
Gustave <gustaverolland777@gmail.com>
2026-07-31 16:50:55 +0200

    updated version for new feature (colour picker)

--- src/main/java/notebookapplication/gui/Controller.java
+++ src/main/java/notebookapplication/gui/Controller.java
@@ -798,7 +798,7 @@ public class Controller implements Initializable, PropertyChangeListener {
     private void handleAbout() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("About NotebookApplication");
-        alert.setHeaderText("NotebookApplication v2.5.3");
+        alert.setHeaderText("NotebookApplication v2.6.3");
 
         Label content = new Label("""
                 A OneNote-like notebook application built with
