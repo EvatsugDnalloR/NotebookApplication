c86534c548802ed47d3e0d6633277a5a4dc5c234
Gustave <gustaverolland777@gmail.com>
2026-07-31 17:06:56 +0200

    update version for bug fix (colour update for mixed colour strings)

--- src/main/java/notebookapplication/gui/Controller.java
+++ src/main/java/notebookapplication/gui/Controller.java
@@ -818,7 +818,7 @@ public class Controller implements Initializable, PropertyChangeListener {
     private void handleAbout() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("About NotebookApplication");
-        alert.setHeaderText("NotebookApplication v2.6.3");
+        alert.setHeaderText("NotebookApplication v2.6.4");
 
         Label content = new Label("""
                 A OneNote-like notebook application built with
